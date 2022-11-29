d0d24421e1211926de5a696f61d5d1d2a4ef32d9
leegwanhee03 <114061618+leegwanhee03@users.noreply.github.com>
2022-11-29 22:53:06 +0900

    Add files via upload

---
+++ 12주차 프기실 과제.c
@@ -0,0 +1,31 @@
+﻿
+Microsoft Visual Studio Solution File, Format Version 12.00
+# Visual Studio Version 17
+VisualStudioVersion = 17.3.32825.248
+MinimumVisualStudioVersion = 10.0.40219.1
+Project("{8BC9CEB8-8B4A-11D0-8D11-00A0C91BC942}") = "프기실 12주차 과제", "..\source\repos\프기실 12주차 과제\프기실 12주차 과제\프기실 12주차 과제.vcxproj", "{FCF21764-11EC-48E3-8F67-2C9A4A9B486D}"
+EndProject
+Global
+	GlobalSection(SolutionConfigurationPlatforms) = preSolution
+		Debug|x64 = Debug|x64
+		Debug|x86 = Debug|x86
+		Release|x64 = Release|x64
+		Release|x86 = Release|x86
+	EndGlobalSection
+	GlobalSection(ProjectConfigurationPlatforms) = postSolution
+		{FCF21764-11EC-48E3-8F67-2C9A4A9B486D}.Debug|x64.ActiveCfg = Debug|x64
+		{FCF21764-11EC-48E3-8F67-2C9A4A9B486D}.Debug|x64.Build.0 = Debug|x64
+		{FCF21764-11EC-48E3-8F67-2C9A4A9B486D}.Debug|x86.ActiveCfg = Debug|Win32
+		{FCF21764-11EC-48E3-8F67-2C9A4A9B486D}.Debug|x86.Build.0 = Debug|Win32
+		{FCF21764-11EC-48E3-8F67-2C9A4A9B486D}.Release|x64.ActiveCfg = Release|x64
+		{FCF21764-11EC-48E3-8F67-2C9A4A9B486D}.Release|x64.Build.0 = Release|x64
+		{FCF21764-11EC-48E3-8F67-2C9A4A9B486D}.Release|x86.ActiveCfg = Release|Win32
+		{FCF21764-11EC-48E3-8F67-2C9A4A9B486D}.Release|x86.Build.0 = Release|Win32
+	EndGlobalSection
+	GlobalSection(SolutionProperties) = preSolution
+		HideSolutionNode = FALSE
+	EndGlobalSection
+	GlobalSection(ExtensibilityGlobals) = postSolution
+		SolutionGuid = {453E1C66-E571-4FE9-AAA0-46C47E5AEEE3}
+	EndGlobalSection
+EndGlobal
